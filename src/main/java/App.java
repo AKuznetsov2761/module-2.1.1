71853341acfabbbf929ecde962e0fa1052b6058b
AKuznetsov2761 <akuznetsov2761@gmail.com>
2021-12-30 12:51:31 +0300

    Final commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,8 +9,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean1);
 
         Cat beanCat = (Cat) applicationContext.getBean("cat");
         System.out.println(beanCat.getMessage());
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(beanCat == beanCat1);
     }
 }
